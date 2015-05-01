4430e502c52adbe04afb4aebfcb640055b0ee652
SHAITA <walid.shaita@gmail.com>
2015-05-01 15:38:02 +0200

    Calendar.h

--- agenda.cpp
+++
@@ -1,7 +0,0 @@
-#include "agenda.h"
-
-//ce sera l'interface graphique de l'emploi du temps
-//A faire
-agenda::agenda(QWidget *parent):QWidget(parent){
-    buttontest= new QPushButton("test",this);
-}
